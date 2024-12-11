10bc774d49438ee7ce0f0f860fd2bf52df92ecb7
unknown <ahmedalariqi97@gmail.com>
2024-12-11 20:10:13 +0300

    edit table

---
+++ database/migrations/2023_09_07_181155_alter_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('role')->default(1)->after('email');
+            $table->string('phone')->nullable()->after('role');
+            $table->integer('status')->default(1)->after('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+            $table->dropColumn('phone');
+            $table->dropColumn('status');
+        });
+    }
+};
